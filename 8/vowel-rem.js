29503dc8168da2d696d639534bc1373744188e6b
jworley24 <122431997+jworley24@users.noreply.github.com>
2023-04-12 15:45:30 -0700

    Add files via upload

---
+++ 8/vowel-rem.js
@@ -0,0 +1,7 @@
+function shortcut (string) {
+    return string.split('a').join('')
+                 .split('e').join('')
+                 .split('i').join('')
+                 .split('o').join('')
+                 .split('u').join('')
+  }
