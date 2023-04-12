24d16d701e429b4da0ff66a13feb1463d2b0c50a
jworley24 <122431997+jworley24@users.noreply.github.com>
2023-04-12 15:36:07 -0700

    Add files via upload

---
+++ 8/mult-table-num.js
@@ -0,0 +1,7 @@
+function multiTable(number) {
+    let table = ''
+    for(let i =1; i <= 10; i++){
+      table += `${i} * ${number} = ${i*number}${i < 10 ? '\n' : ''}`
+    }
+    return table
+  }
