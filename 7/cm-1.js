77ec3325b724d61055b1e56bcc36166b7f5e2daf
Joseph Worley <122431997+jworley24@users.noreply.github.com>
2023-05-17 10:48:29 -0700

    Add files via upload

---
+++ 7/cm-1.js
@@ -0,0 +1,10 @@
+var list1 = [
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+  ];
+
+  function countDevelopers(list) {
+    return list.filter(el => el.continent == 'Europe' && el.language == 'JavaScript').length
+  }
