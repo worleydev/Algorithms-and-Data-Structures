031819d5a9fa7c59b1b93fc5137f2520b2d7d34f
Joseph Worley <122431997+jworley24@users.noreply.github.com>
2023-07-12 22:46:33 -0700

    Add files via upload

---
+++ 8/determine-offspring.js
@@ -0,0 +1,3 @@
+function chromosomeCheck(sperm) {
+    return sperm[sperm.length - 1] === "Y" ? "Congratulations! You're going to have a son." : "Congratulations! You're going to have a daughter."
+  }
